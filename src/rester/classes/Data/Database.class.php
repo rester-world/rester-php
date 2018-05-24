bc07bbb48315174d7d181ece4defed4346ce031c
kevinpark <kevinpark@webace.co.kr>
2018-05-25 00:41:37 +0900

    datebase timezone setting

--- src/rester/classes/Data/Database.class.php
+++ src/rester/classes/Data/Database.class.php
@@ -34,7 +34,8 @@ class Database extends PDO
             $this->tbn = null;
             parent::__construct($dsn, $user_name, $password);
 
-            $this->exec("set names utf8");
+            $timezone = cfg::Get('default','timezone');
+            $this->exec("set names utf8; SET time_zone='{$timezone}';");
             $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
