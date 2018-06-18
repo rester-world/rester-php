61008db677a7758e2c600b878b3a500bf4477e61
박경종 <kevinpark@webace.co.kr>
2018-06-18 18:48:24 +0900

    File Upload with url
    - 기능 구현완료

--- src/rester/common.lib.php
+++ src/rester/common.lib.php
@@ -250,3 +250,22 @@ function verify_param($key, $function)
     if($data) rester::set_request_param($key, $data);
 }
 
+/**
+ * @param $url string
+ * @param $saveto string
+ */
+function grab_image($url,$saveto){
+    $ch = curl_init ($url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_BINARYTRANSFER,1);
+    $raw=curl_exec($ch);
+    curl_close ($ch);
+    if(file_exists($saveto)){
+        unlink($saveto);
+    }
+    $fp = fopen($saveto,'x');
+    fwrite($fp, $raw);
+    fclose($fp);
+}
+
