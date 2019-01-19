aa5d7681354bc43116d1b9603745c495cc66c026
박경종 <kevinpark@webace.co.kr>
2019-01-19 11:57:04 +0900

    fn 기능은 제거

--- src/rester/common.lib.php
+++ src/rester/common.lib.php
@@ -1,36 +1,13 @@
 <?php if(!defined('__RESTER__')) exit;
 
 /**
- * fn.***.php 파일을 호출
- *
- * @param string $name
- * @param null|string $module
- *
- * @return bool|mixed
- * @throws Exception
- */
-function fn($name, $module=null)
-{
-    $result = false;
-    $old = null;
-    if($module) $old = cfg::change_module($module);
-    if($path = rester::path_fn($name))
-    {
-        /** @noinspection PhpUnusedLocalVariableInspection */
-        $arg = array_slice(func_get_args(),1);
-        $result = include $path;
-    }
-    if($old) cfg::change_module($old);
-    return $result;
-}
-
-/**
  * @param array $arr
  *
  * @return bool
  */
 function is_assoc($arr)
 {
-    $keys = array_keys($arr);
-    return array_keys($keys) !== $keys;
+    $res = false;
+    foreach($arr as $k=>$v) if(!is_numeric($k)) $res = true;
+    return $res;
 }
