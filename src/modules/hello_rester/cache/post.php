5e5f8f0158b894beb258c44bfb1b1208c6ff67ef
박경종 <kevinpark@webace.co.kr>
2019-04-16 18:43:34 +0900

    예제 업데이트

--- src/modules/hello_rester/cache/post.php
+++ src/modules/hello_rester/cache/post.php
@@ -1,8 +1,8 @@
 <?php if(!defined('__RESTER__')) exit;
 
-rester_response::msg("Hello RESTer-SQL world!");
+rester_response::msg("Cached!");
 return [
     'Cache example (5 second)',
-    'Current Datetime'.date("Y-m-d H:i:s")
+    'Current Datetime '.date("Y-m-d H:i:s")
 ];
 
--- src/modules/hello_rester/verify/post.verify.php
+++ src/modules/hello_rester/verify/post.verify.php
@@ -4,8 +4,3 @@ function test_user_func($value)
 {
     return strpos($value, 'rester')===false?false:$value;
 };
-
-function x_auth_user_func($value)
-{
-    return strpos($value, 'rester')===false?false:$value;
-};
